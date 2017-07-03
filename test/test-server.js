5be58f1fe76973917a63fd478901c5649c0e2d13
John Garavito Suarez <jfreddy@br.ibm.com>
2017-07-03 18:07:07 -0300

    adding testing and VUE

--- test/test-server.js
+++ test/test-server.js
@@ -5,6 +5,166 @@ var should = chai.should();
 
 chai.use(chaiHttp);
 
+describe('Tasks', function() {
+  it('should list ALL Tasks on /api/tasks GET', function(done) {
+    chai.request(server)
+      .get('/api/tasks')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.should.be.length(0);
+        done();
+      });
+  });
+  it('should list a SINGLE task on /api/tasks/<id> GET', function(done){
+    var Task = server.models.Task;
+    var task = {'name': 'Task 1', 'duration': 1};
+    Task.create(task, function(err, obj) {
+      chai.request(server)
+        .get('/api/tasks/' + obj.id)
+        .end(function(err, res) {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property('id');
+          res.body.should.have.property('name');
+          res.body.should.have.property('duration');
+          res.body.name.should.equal('Task 1');
+          res.body.duration.should.equal(1);
+          done();
+        });
+    });
+  });
+  it('should add a SINGLE task on /api/tasks POST', function(done){
+    chai.request(server)
+      .post('/api/tasks')
+      .send({'name': 'Task 1', 'duration': 1})
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('id');
+        res.body.should.have.property('name');
+        res.body.should.have.property('duration');
+        done();
+      });
+  });
+  it('should update a SINGLE task on /api/tasks/<id> PUT', function(done) {
+    chai.request(server)
+      .get('/api/tasks')
+      .end(function(err, res) {
+        chai.request(server)
+          .put('/api/tasks/'+res.body[0].id)
+          .send({'name': 'Task 1', 'duration': 2, 'id': 1})
+          .end(function(err, res) {
+            res.should.have.status(200);
+            res.should.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property('id');
+            res.body.should.have.property('name');
+            done();
+          });
+      });
+  });
+  it('should delete a SINGLE task on /api/tasks/<id> DELETE', function(done) {
+    chai.request(server)
+      .get('/api/tasks')
+      .end(function(err, res) {
+        chai.request(server)
+          .delete('/api/tasks/'+res.body[0].id)
+          .end(function(err, res){
+            res.should.have.status(200);
+            res.should.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property('count');
+            res.body.count.should.equal(1);
+            done();
+          });
+      });
+  });
+});
+
+describe('Technicians', function() {
+  it('should list ALL Technicians on /api/technicians GET', function(done) {
+    chai.request(server)
+      .get('/api/technicians')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.should.be.length(0);
+        done();
+      });
+  });
+  it('should list a SINGLE technician on /api/technicians/<id> GET', function(done) {
+    var Technician = server.models.Technician;
+    var technician = {'name': 'Technician 1', 'initial_time': 8, 'final_time': 18};
+    Technician.create(technician, function(err, obj) {
+     chai.request(server)
+     .get('/api/technicians/' + obj.id)
+     .end(function(err, res) {
+       res.should.have.status(200);
+       res.should.be.json;
+       res.body.should.be.a('object');
+       res.body.should.have.property('id');
+       res.body.should.have.property('name');
+       res.body.name.should.equal('Technician 1');
+       res.body.initial_time.should.equal(8);
+       res.body.final_time.should.equal(18);
+       done();
+     });
+   });
+  });
+  it('should add a SINGLE technician on /api/technicians POST', function(done) {
+    chai.request(server)
+      .post('/api/technicians')
+      .send({'name': 'Technician 1', 'initial_time': 8, 'final_time': 18})
+      .end(function (err, res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('id');
+        res.body.should.have.property('initial_time');
+        res.body.should.have.property('final_time');
+        done();
+      });
+  });
+  it('should update a SINGLE technician on /api/technicians/<id> PUT', function(done) {
+   chai.request(server)
+   .get('/api/technicians')
+   .end(function(err, res) {
+   chai.request(server)
+   .put('/api/technicians/'+res.body[0].id)
+   .send({'name': 'Technician 1', 'initial_time': 8, 'final_time': 18, 'id': 1})
+   .end(function(err, res) {
+   res.should.have.status(200);
+   res.should.be.json;
+   res.body.should.be.a('object');
+   res.body.should.have.property('id');
+   res.body.should.have.property('name');
+   done();
+   });
+   });
+   });
+  it('should delete a SINGLE task on /api/tasks/<id> DELETE', function(done) {
+    chai.request(server)
+      .get('/api/tasks')
+      .end(function (err, res) {
+        chai.request(server)
+          .delete('/api/tasks/' + res.body[0].id)
+          .end(function (err, res) {
+            res.should.have.status(200);
+            res.should.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property('count');
+            res.body.count.should.equal(1);
+            done();
+          });
+      });
+  });
+});
+
 describe('Notes', function() {
   it('should list ALL Notes on /api/notes GET', function(done) {
     chai.request(server)
