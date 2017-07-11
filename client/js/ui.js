2cb72fea81d4959ce995138c9fb7425445248ee2
John Garavito Suarez <jfreddy@br.ibm.com>
2017-07-10 23:45:01 -0300

    Adding http methods

--- client/js/ui.js
+++ client/js/ui.js
@@ -1,7 +1,97 @@
+var http = {
+  "post": function (url, requestParams) {
+    return new Promise(function (resolve, reject) {
+      if (!requestParams) {
+        return reject("Can not proceed without credentials");
+      }
+
+      if (window.XMLHttpRequest) {
+        var xhttp = new window.XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (xhttp.readyState === 4) {
+            if (xhttp.status === 200 || xhttp.status === 201) {
+              if (xhttp.responseText) {
+                try {
+                  resolve(JSON.parse(xhttp.responseText));
+                } catch (e) {
+                  resolve(xhttp.responseText);
+                }
+              } else {
+                reject("Empty response");
+              }
+
+            } else {
+              reject(xhttp.responseText);
+            }
+          }
+        };
+
+        xhttp.open("POST", url);
+        xhttp.setRequestHeader("content-type", "application/json");
+        xhttp.send(JSON.stringify(requestParams));
+
+      } else {
+        reject("AJAX Calls not supported on this browser");
+      }
+    });
+  },
+  "get": function (url) {
+    return new Promise(function (resolve, reject) {
+      if (window.XMLHttpRequest) {
+        var xhttp = new window.XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (xhttp.readyState === 4) {
+            if (xhttp.status === 200) {
+              if (xhttp.responseText) {
+                try {
+                  resolve(JSON.parse(xhttp.responseText));
+                } catch (e) {
+                  resolve(xhttp.responseText);
+                }
+              } else {
+                reject("Empty response");
+              }
+
+            } else {
+              reject(xhttp.responseText);
+            }
+          }
+        };
+
+        xhttp.open("GET", url);
+        xhttp.send();
+      } else {
+        reject("AJAX Calls not supported on this browser");
+      }
+    });
+  }
+}
+
+http.get("/api/tasks")
+  .then(function(tasks){
+    var tasksList = document.getElementById("tasksList");
+    for(var id in tasks){
+      var node = document.createElement("p");
+      var textnode = document.createTextNode(tasks[id].name + tasks[id].duration);
+      node.appendChild(textnode);
+      tasksList.append(node);
+    }
+
+  });
+
 var tasksInput = new Vue({
   el: '#tasksInput',
   data: {
-    message: 'Hello Vue!'
+    name: "",
+    duration: 1
+  },
+  methods: {
+    createTask: function (event) {
+      http.post("/api/tasks", {"name": this.name, "duration": this.duration})
+      .then(function(obj){
+        console.log(obj)
+      });
+    }
   }
 });
 
