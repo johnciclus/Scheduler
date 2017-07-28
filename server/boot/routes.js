9201f13635310189c372dec571d590f17883aaa0
John Garavito Suarez <jfreddy@br.ibm.com>
2017-07-27 23:00:43 -0300

    Adding map route

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -2,4 +2,8 @@ module.exports = function(app) {
 	app.get("/", function(req, res) {
 		res.redirect("./main_module/index.html");
 	});
+
+	app.get("/maps", function(req, res) {
+		res.redirect("./maps_module/index.html");
+	});
 }
